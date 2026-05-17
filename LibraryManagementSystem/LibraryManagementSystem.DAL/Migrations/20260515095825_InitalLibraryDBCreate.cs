c22534642c5e30712b4dd89ed861f89efc807657
nandhiraja <nandhiraja16@gmail.com>
2026-05-17 22:56:52 +0530

    chore: add other support files

---
+++ LibraryManagementSystem/LibraryManagementSystem.DAL/Migrations/20260515095825_InitalLibraryDBCreate.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace LibraryManagementSystem.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitalLibraryDBCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MembershipTypes",
+                columns: table => new
+                {
+                    MembershipTypeId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    MaxBooks = table.Column<int>(type: "integer", nullable: false),
+                    MaxDays = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MembershipTypes", x => x.MembershipTypeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BookTitles",
+                columns: table => new
+                {
+                    BookTitleId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Author = table.Column<string>(type: "text", nullable: false),
+                    PublishedYear = table.Column<int>(type: "integer", nullable: false),
+                    CategoryId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookTitles", x => x.BookTitleId);
+                    table.ForeignKey(
+                        name: "FK_BookTitles_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "CategoryId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Members",
+                columns: table => new
+                {
+                    MemberId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Phone = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false),
+                    MemberRole = table.Column<int>(type: "integer", nullable: false),
+                    MembershipTypeId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Members", x => x.MemberId);
+                    table.ForeignKey(
+                        name: "FK_Members_MembershipTypes_MembershipTypeId",
+                        column: x => x.MembershipTypeId,
+                        principalTable: "MembershipTypes",
+                        principalColumn: "MembershipTypeId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BookCopies",
+                columns: table => new
+                {
+                    BookCopyId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CopyCode = table.Column<string>(type: "text", nullable: false),
+                    Status = table.Column<string>(type: "text", nullable: false),
+                    BookTitleId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookCopies", x => x.BookCopyId);
+                    table.ForeignKey(
+                        name: "FK_BookCopies_BookTitles_BookTitleId",
+                        column: x => x.BookTitleId,
+                        principalTable: "BookTitles",
+                        principalColumn: "BookTitleId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BorrowTransactions",
+                columns: table => new
+                {
+                    BorrowId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    MemberId = table.Column<int>(type: "integer", nullable: false),
+                    BookCopyId = table.Column<int>(type: "integer", nullable: false),
+                    BorrowDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    DueDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ReturnDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    Status = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BorrowTransactions", x => x.BorrowId);
+                    table.ForeignKey(
+                        name: "FK_BorrowTransactions_BookCopies_BookCopyId",
+                        column: x => x.BookCopyId,
+                        principalTable: "BookCopies",
+                        principalColumn: "BookCopyId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_BorrowTransactions_Members_MemberId",
+                        column: x => x.MemberId,
+                        principalTable: "Members",
+                        principalColumn: "MemberId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Fines",
+                columns: table => new
+                {
+                    FineId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    BorrowId = table.Column<int>(type: "integer", nullable: false),
+                    Amount = table.Column<decimal>(type: "numeric(10,2)", nullable: false),
+                    IsPaid = table.Column<bool>(type: "boolean", nullable: false),
+                    PaidDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fines", x => x.FineId);
+                    table.ForeignKey(
+                        name: "FK_Fines_BorrowTransactions_BorrowId",
+                        column: x => x.BorrowId,
+                        principalTable: "BorrowTransactions",
+                        principalColumn: "BorrowId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookCopies_BookTitleId",
+                table: "BookCopies",
+                column: "BookTitleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookCopies_CopyCode",
+                table: "BookCopies",
+                column: "CopyCode",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookTitles_CategoryId",
+                table: "BookTitles",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BorrowTransactions_BookCopyId",
+                table: "BorrowTransactions",
+                column: "BookCopyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BorrowTransactions_MemberId",
+                table: "BorrowTransactions",
+                column: "MemberId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Fines_BorrowId",
+                table: "Fines",
+                column: "BorrowId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Members_Email",
+                table: "Members",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Members_MembershipTypeId",
+                table: "Members",
+                column: "MembershipTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Members_Phone",
+                table: "Members",
+                column: "Phone",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Fines");
+
+            migrationBuilder.DropTable(
+                name: "BorrowTransactions");
+
+            migrationBuilder.DropTable(
+                name: "BookCopies");
+
+            migrationBuilder.DropTable(
+                name: "Members");
+
+            migrationBuilder.DropTable(
+                name: "BookTitles");
+
+            migrationBuilder.DropTable(
+                name: "MembershipTypes");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
